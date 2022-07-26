371f714b586099253176679b7d808d8293c2e34e
alvaroormeno <alvaro@ormeno.org>
2022-07-26 09:47:06 -0400

    step-4 setup static files complete, created public, css folders and scss file

--- app.js
+++ app.js
@@ -10,13 +10,14 @@ const app = express();
 // Step 1-C (if we want to publish app we can use environment port number or use default port 5001)
 const port = process.env.PORT || 5001;
 
-// Step 3
+// Step 3 - Setup Body Parser
 // parse application/x-www-form-urlecoded
 app.use(bodyParser.urlencoded({extended: false}));
 // parse application/json (make sure body parser is using json)
 app.use(bodyParser.json()); 
 
-
+// Step 4 - Setup Static Files
+app.use(express.static('public'));
 
 
 
@@ -44,3 +45,6 @@ app.listen(port, () => {
 //
 // STEP 3 - Setup BodyParser 
 // will help us pass json data through our forms
+//
+// STEP 4 - Setup static files
+// the files we want to add in our front end, like a custom css file or js file or an easy acess to images
