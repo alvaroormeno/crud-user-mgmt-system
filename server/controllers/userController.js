4d11cb65088c83aa0c4e3f9362ba0c91d51beae5
alvaroormeno <alvaro@ormeno.org>
2022-08-04 20:48:27 -0400

    edit user page is now rendering user info

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -98,6 +98,24 @@ exports.create = (req, res) => {
 // Edit User
 exports.edit = (req, res) => {
 
-    res.render('edit-user');
+    pool.getConnection((err, connection) => {
+        if(err) throw err; //not connected!
+        console.log('Connected as ID' + connection.threadId); 
+
+        // Use the connection - query to view all users that only have active status in database user table
+        connection.query('SELECT * FROM user WHERE id = ?', [req.params.id], (err, rows) => {
+            // When done with the connection, release it
+            connection.release()
+
+            if(!err) {
+                res.render('edit-user', {rows});
+            } else {
+                console.log(err);
+            }
+
+            console.log('The data from user table: \n', rows)
+        });
+    })
+    
 
 }
