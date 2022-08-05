016b041c30140c1d722c37ae29d42682dcba2787
alvaroormeno <alvaro@ormeno.org>
2022-08-04 22:54:03 -0400

    created view user controller

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -9,7 +9,7 @@ const pool = mysql.createPool({
 });
 
 
-// View Users
+// View All Users
 exports.view = (req, res) => {
     pool.getConnection((err, connection) => {
         if(err) throw err; //not connected!
@@ -213,3 +213,26 @@ exports.delete = (req, res) => {
     
 
 }
+
+
+// View User
+exports.view = (req, res) => {
+    pool.getConnection((err, connection) => {
+        if(err) throw err; //not connected!
+        console.log('Connected as ID' + connection.threadId); 
+
+        // Use the connection - query to view all users that only have active status in database user table
+        connection.query('SELECT * FROM user WHERE id = ?' [req.params.id], (err, rows) => {
+            // When done with the connection, release it
+            connection.release()
+
+            if(!err) {
+                res.render('view-user', {rows});
+            } else {
+                console.log(err);
+            }
+
+            console.log('The data from user table: \n', rows)
+        });
+    })
+};
